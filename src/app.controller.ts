acc4ae32cc45534cfbb89021e3ff98c58fcb7f42
Matheus <matheus.rj25@hotmail.com>
2022-12-16 18:44:47 -0300

    config initial

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
-@Controller()
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prisma.service';
+import { Body, Controller, Get,Post } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list(){
+    return this.prisma.notification.findMany();
+  }
+  @Post()
+  create(@Body() body:CreateNotificationBody){
+    const {recipientId, content, category} = body;
+    return this.prisma.notification.create({
+      data:{
+        id:randomUUID(),
+        content,
+        category,
+        recipientId,
+      } 
+    });
   }
 }
